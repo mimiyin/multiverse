9f5f35ffe2698b9537466232fad5ffa87dca0d50
Mimi Yin <mimi@parlor.is>
2013-09-16 20:27:58 -0400

    back to basics

--- wsgi.py
+++ wsgi.py
@@ -13,22 +13,13 @@ middleware here, or combine a Django application with an application of another
 framework.
 
 """
-import os, sys, site
+import os
 
 # We defer to a DJANGO_SETTINGS_MODULE already in the environment. This breaks
 # if running multiple sites in the same mod_wsgi process. To fix this, use
 # mod_wsgi daemon mode with each site in its own daemon process, or use
 # os.environ["DJANGO_SETTINGS_MODULE"] = "multiverse.settings"
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "madtwitter.settings")
-
-# Calculate the path based on the location of the WSGI script
-project = '/home/capps/webapps/multiverse/madtwitter'
-workspace = os.path.dirname(project)
-sys.path.append(workspace)
-
-# Add to sys path
-sys.path = ['/home/capps/webapps/multiverse', '/home/capps/webapps/multiverse/madtwitter'] + sys.path
-
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
